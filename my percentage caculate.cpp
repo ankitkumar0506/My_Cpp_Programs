e91c47a600dfe5269425888f1b29f2918ef8d4d0
Ankit Kumar <138195508+ankitkumar0506@users.noreply.github.com>
2023-07-31 10:33:24 +0530

    Add files via upload

---
+++ my percentage caculate.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ 
+	int m1 = 100;  //  MARHTS MARKS
+	int m2 = 77;  //  PHYSICS MARKS
+	int m3 = 92;  //  CHEKSTRY MARKS
+	int m4 = 90;  //  ENGLISH MARKS
+	int m5 = 89;  //  ECONOMICS MARKS
+	int p = (m1 + m2 + m3 + m4 + m5)/5;
+	printf("percentage of 5 subjects is %d",p);
+}
---
+++ name.cpp
@@ -0,0 +1,25 @@
+
+
+#include<stdio.h>
+
+		int main(){
+			
+		int name,birth,age,add;
+		
+			
+		printf(" ENTER YOUR NAME: ");
+		scanf("%d",&name);
+		
+		printf(" ENTER YOUR BIRTH DATE: ");	
+		scanf("%d",&birth);
+	
+		printf(" ENTER YOUR AGE: ");
+		scanf("%d",&age);
+		
+	
+		printf(" ENTER YOUR ADDRESS: ");
+		scanf("%d",&add),
+	
+	//printf("x = %d  y  = %d z = %d", x, y,z);
+	
+}
